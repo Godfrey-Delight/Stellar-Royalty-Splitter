6b2055aa4a2bca867943c63abd38057053e94888
Owoh Chidubem Alexander <chidubempaul91@gmail.com>
2026-04-23 12:51:51 +0100

    chore: remove accidentally committed JS files and update tsconfig

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
-import "./modern-styles.css";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(ThemeProvider, { children: _jsx(App, {}) }) }));
